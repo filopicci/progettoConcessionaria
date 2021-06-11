0525fd9f35b0ff2174e6fe48c3b920ad20682c40
Filippo <filo.picci@gmail.com>
2021-06-11 11:29:45 +0200

    Initial commit

---
+++ src/main/java/it/nttdata/progettoconcessionaria/ProgettoConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package it.nttdata.progettoconcessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoConcessionariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoConcessionariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/nttdata/progettoconcessionaria/ProgettoConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nttdata.progettoconcessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoConcessionariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
