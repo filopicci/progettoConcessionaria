31de96d4fec1f87b91e413a3a543115b55cd8e9c
Filippo <filo.picci@gmail.com>
2021-06-11 17:59:11 +0200

    aggiunta di lista auto

--- src/main/java/it/nttdata/progettoconcessionaria/controllers/AutoController.java
+++ src/main/java/it/nttdata/progettoconcessionaria/controllers/AutoController.java
@@ -1,9 +1,15 @@
 package it.nttdata.progettoconcessionaria.controllers;
 
+import it.nttdata.progettoconcessionaria.models.Auto;
+import it.nttdata.progettoconcessionaria.models.Concessionario;
 import it.nttdata.progettoconcessionaria.repository.AutoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+
 
 @Controller
 public class AutoController {
@@ -18,6 +24,19 @@ public class AutoController {
     @GetMapping("/listaauto")
     public String getAuto(Model model){
         model.addAttribute( "automobili" , autoRepository.findAll());
+        return "listaauto";
+    }
+
+    @GetMapping("/listaauto/{nomeConcessionario}")
+    public String getListaAutoConcessionario(Model model,@PathVariable("nomeConcessionario") String nomeConcessionario){
+        ArrayList<Auto> listaAuto = (ArrayList<Auto>) autoRepository.findAll();
+        ArrayList<Auto> listaAutoFiltrate = new ArrayList<>();
+        for(Auto auto : listaAuto){
+            if(auto.getConcessionario().getNome().equals(nomeConcessionario)) {
+                listaAutoFiltrate.add(auto);
+            }
+        }
+        model.addAttribute("automobili", listaAutoFiltrate);
         return "listaauto";
     }
 }
--- src/main/java/it/nttdata/progettoconcessionaria/repository/AutoRepository.java
+++ src/main/java/it/nttdata/progettoconcessionaria/repository/AutoRepository.java
@@ -1,7 +1,9 @@
 package it.nttdata.progettoconcessionaria.repository;
 
 import it.nttdata.progettoconcessionaria.models.Auto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AutoRepository extends CrudRepository<Auto, Long> {
+
 }
